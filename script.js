3058526edf2b7f11cfbc57a9ced462bf3cc40c52
sampath <sampathsampath0852@gmail.com>
2022-08-09 21:56:22 +0530

    added basic ui

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-
-let playerwon = 0;
-let computerwon = 0;
-
+const rules = {
+  'rock': 'scissors',
+  'scissors': 'paper',
+  'paper': 'rock'
+}
 
 function getComputerChoice() {
-    const compchoice = ['rock' , 'paper' , 'scissors'];
-    return (compchoice[Math.floor(Math.random() * 3)])
+  const compchoice = ['rock', 'paper', 'scissors'];
+  return (compchoice[Math.floor(Math.random() * 3)])
 }
-function singleround(computerChoice, playerChoice){
-      const rules = {
-        'rock':'scissors',
-        'scissors':'paper',
-        'paper':'rock'
-      }
-      if(computerChoice === playerChoice){
-        computerwon++;
-        playerwon++;
-      }else{
-        if(rules[computerChoice] === playerChoice) {
-         computerwon++;
-        } 
-         playerwon++;    
-      }
 
-}
-function game(n){ 
-  for(let i = 0; i < n; i++ ){
-    let computerChoice = getComputerChoice();
-    let playerChoice = prompt('enter your choice:');
-    singleround(computerChoice,playerChoice);
-    
+
+function singleround(computerChoice, playerChoice) {
+
+  if (computerChoice === playerChoice) {
+    computerwon++;
+    playerwon++;
+  } else {
+    if (rules[computerChoice] === playerChoice) {
+      computerwon++;
+    }
+    playerwon++;
   }
 }
-game(1)
-if(playerwon === computerwon){
-      alert('its a draw');
-}else{
-      (playerwon > computerwon) ? alert(`you score is ${playerwon},you won`) : alert(`your score is ${playerwon},you lose`);
-}
+
+
+
+let button = document.querySelectorAll('button');
+
+button.forEach(element => {
+  element.addEventListener('click', () => {
+    alert('hello world')
+  })
+});
