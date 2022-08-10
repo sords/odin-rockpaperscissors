ce9446d0ac605d0aa3e522fd98c4a154f82e19f7
sampath <sampathsampath0852@gmail.com>
2022-08-10 22:01:11 +0530

    done javascript logic

--- script.js
+++ script.js
@@ -9,26 +9,39 @@ function getComputerChoice() {
   return (compchoice[Math.floor(Math.random() * 3)])
 }
 
+let button = document.querySelectorAll('button');
+let plyerchoice;
+let text = document.querySelector('.text');
+//let playerscore = document.querySelector('.payerscore');
+//let computerscore = document.querySelector('.computerscore')
+let score1 = 0;
+let score2 = 0;
+
+button.forEach(element => {
+  element.addEventListener('click', () => {
+    plyerchoice = element.getAttribute('id');
+    singleround(getComputerChoice(),plyerchoice)
+  })
+})
 
 function singleround(computerChoice, playerChoice) {
+  console.log(computerChoice,playerChoice)
 
   if (computerChoice === playerChoice) {
-    computerwon++;
-    playerwon++;
+      text.textContent = "it's a tie my dear";
   } else {
     if (rules[computerChoice] === playerChoice) {
-      computerwon++;
+      text.textContent = "it's time to take control the world";
+      score1++;
+      document.getElementById('playerscore').innerText = score1;
+
+    } else {
+      text.textContent = 'i think human owns this';
+      console.log(computerscore.value);
+      score2++;
+      document.getElementById('computerscore').innerText = score2;
     }
-    playerwon++;
   }
 }
 
 
-
-let button = document.querySelectorAll('button');
-
-button.forEach(element => {
-  element.addEventListener('click', () => {
-    alert('hello world')
-  })
-});
