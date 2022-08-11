434f5681a853f5dd22e3c765224aad8fb4ff4997
sampath <sampathsampath0852@gmail.com>
2022-08-11 22:30:02 +0530

    man i am lazy

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getComputerChoice() {
   return (compchoice[Math.floor(Math.random() * 3)])
 }
 
-let button = document.querySelectorAll('button');
+let button = document.querySelectorAll('.button');
 let plyerchoice;
 let text = document.querySelector('.text');
 //let playerscore = document.querySelector('.payerscore');
@@ -20,15 +20,28 @@ let score2 = 0;
 button.forEach(element => {
   element.addEventListener('click', () => {
     plyerchoice = element.getAttribute('id');
-    singleround(getComputerChoice(),plyerchoice)
+    singleround(getComputerChoice(), plyerchoice)
   })
 })
+function refreshpage() {
+  let comentry = document.querySelector('.commentry');
+  let playagaing = document.createElement('button');
+  playagaing.setAttribute('id', 'playagain'); 
+  let refresh = document.querySelector('#playagain')
+
+  comentry.removeChild(text)
+  comentry.appendChild(playagaing)
+  document.getElementById('playagain').innerText = 'Play again';
+  comentry.addEventListener('click', window.location.reload(true))
+}
 
 function singleround(computerChoice, playerChoice) {
-  console.log(computerChoice,playerChoice)
+  console.log(computerChoice, playerChoice)
+  if (score1 === 5 || score2 === 5) {
+    refreshpage();
 
-  if (computerChoice === playerChoice) {
-      text.textContent = "it's a tie my dear";
+  } else if (computerChoice === playerChoice) {
+    text.textContent = "it's a tie my dear";
   } else {
     if (rules[computerChoice] === playerChoice) {
       text.textContent = "it's time to take control the world";
@@ -41,6 +54,8 @@ function singleround(computerChoice, playerChoice) {
       score2++;
       document.getElementById('computerscore').innerText = score2;
     }
+
+
   }
 }
 
