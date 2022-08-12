093bd46de9d9d17bd606e1bcd97feb94a41a684f
sampath <sampathsampath0852@gmail.com>
2022-08-12 10:58:30 +0530

    finished project

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let text = document.querySelector('.text');
 //let computerscore = document.querySelector('.computerscore')
 let score1 = 0;
 let score2 = 0;
-
+let comentry = document.querySelector('.commentry');
 button.forEach(element => {
   element.addEventListener('click', () => {
     plyerchoice = element.getAttribute('id');
@@ -24,15 +24,18 @@ button.forEach(element => {
   })
 })
 function refreshpage() {
-  let comentry = document.querySelector('.commentry');
-  let playagaing = document.createElement('button');
-  playagaing.setAttribute('id', 'playagain'); 
-  let refresh = document.querySelector('#playagain')
 
-  comentry.removeChild(text)
-  comentry.appendChild(playagaing)
-  document.getElementById('playagain').innerText = 'Play again';
-  comentry.addEventListener('click', window.location.reload(true))
+  let refresh = document.getElementById('refresh');
+  text.textContent = 'PLAY AGAIN';
+  refresh.addEventListener('click', () => {
+    window.location.reload(true)
+  })
+  let repress = document.getElementById('repress')
+  let btn = document.createElement('button');
+ btn.setAttribute('id','refresh1')
+ repress.appendChild(btn)
+ repress.getElementById('refresh1').innerText = 'PLAY AGAIN';
+
 }
 
 function singleround(computerChoice, playerChoice) {
@@ -47,12 +50,20 @@ function singleround(computerChoice, playerChoice) {
       text.textContent = "it's time to take control the world";
       score1++;
       document.getElementById('playerscore').innerText = score1;
+      if (score1 === 5) {
+        text.textContent = 'player have won'
+        //refreshpage()
+      }
 
     } else {
       text.textContent = 'i think human owns this';
       console.log(computerscore.value);
       score2++;
       document.getElementById('computerscore').innerText = score2;
+      if (score2 === 5) {
+        text.textContent = 'compu have won'
+        //refreshpage()
+      }
     }
 
 
